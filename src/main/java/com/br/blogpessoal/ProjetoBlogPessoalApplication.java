536f6877b4f1e756a168ae601dddd3e56023f217
nathaliacrz <nathalias161@gmail.com>
2022-07-10 11:31:20 -0300

    Initial commit

---
+++ src/main/java/com/br/blogpessoal/ProjetoBlogPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.br.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBlogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBlogPessoalApplication.class, args);
+	}
+
+}
