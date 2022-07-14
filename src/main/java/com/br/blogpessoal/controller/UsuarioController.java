e02089bc2860d90b553784b3be6ac3219925ba2b
nathaliacrz <nathalias161@gmail.com>
2022-07-14 15:59:27 -0300

    implementando testes com JUnit

--- src/main/java/com/br/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/br/blogpessoal/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.br.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.br.blogpessoal.model.UserLogin;
 import com.br.blogpessoal.model.Usuario;
+import com.br.blogpessoal.repository.UsuarioRepository;
 import com.br.blogpessoal.service.UsuarioService;
 
 @RestController
@@ -21,19 +25,33 @@ import com.br.blogpessoal.service.UsuarioService;
 public class UsuarioController {
 
 	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Autowired
 	private UsuarioService usuarioService;
-	
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+
+		return ResponseEntity.ok(usuarioRepository.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return usuarioRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
 	@PostMapping("/logar")
-	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
-		return usuarioService.logarUsuario(user)
-				.map(resp -> ResponseEntity.ok(resp))
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
+		return usuarioService.logarUsuario(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
-	
+
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Optional<Usuario>> post(@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrarUsuario(usuario));
 	}
-	
+
 }
-	
--- src/main/java/com/br/blogpessoal/model/Usuario.java
+++ src/main/java/com/br/blogpessoal/model/Usuario.java
@@ -1,12 +1,18 @@
 package com.br.blogpessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -15,7 +21,7 @@ public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotBlank
 	@Size(min = 2, max = 100)
 	private String nome;
@@ -28,7 +34,22 @@ public class Usuario {
 
 	@Size(min = 5, max = 100)
 	private String senha;
-	
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+
+	public Usuario() {
+	}
+
 	public long getId() {
 		return id;
 	}
@@ -64,8 +85,16 @@ public class Usuario {
 	public String getFoto() {
 		return foto;
 	}
-	
+
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 }
